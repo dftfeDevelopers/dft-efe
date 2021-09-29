c4122e5ab60574807cb8fb9cb911b3210c15c1f4
Sambit Das <dsambit@umich.edu>
2021-09-29 17:53:00 -0400

    created blasTemplates.h and fixed compilation with private linking to blas lapack

--- src/linearAlgebra/Vector.cpp
+++ src/linearAlgebra/Vector.cpp
@@ -1,9 +1,40 @@
 #include "Vector.h"
-
+#include "blasTemplates.h"
 
 namespace dftefe
 {
+  template <typename NumberType, MemorySpace memorySpace>
+  void
+  Vector<NumberType, memorySpace>::testDgemv()
+  {
+    char                transA = 'N';
+    char                transB = 'T';
+    const double        alpha = 1.0, beta = 0.0;
+    unsigned int        numberWaveFunctions = 10;
+    unsigned int        numberDofs          = 10;
+    std::vector<double> X, Y, output;
+    X.resize(100);
+    Y.resize(100);
+    output.resize(100);
+
+
+
+    dgemm_(&transA,
+           &transB,
+           &numberWaveFunctions,
+           &numberWaveFunctions,
+           &numberDofs,
+           &alpha,
+           &X[0],
+           &numberWaveFunctions,
+           &Y[0],
+           &numberWaveFunctions,
+           &beta,
+           &output[0],
+           &numberWaveFunctions);
+  }
+
   template class Vector<double, MemorySpace::HOST>;
 
 
-}
+} // namespace dftefe
--- src/linearAlgebra/Vector.h
+++ src/linearAlgebra/Vector.h
@@ -5,38 +5,6 @@
 #include <vector>
 namespace dftefe
 {
-  extern "C"
-  {
-    void
-    dgemv_(const char *        TRANS,
-           const unsigned int *M,
-           const unsigned int *N,
-           const double *      alpha,
-           const double *      A,
-           const unsigned int *LDA,
-           const double *      X,
-           const unsigned int *INCX,
-           const double *      beta,
-           double *            C,
-           const unsigned int *INCY);
-
-    void
-    dgemm_(const char *        transA,
-           const char *        transB,
-           const unsigned int *m,
-           const unsigned int *n,
-           const unsigned int *k,
-           const double *      alpha,
-           const double *      A,
-           const unsigned int *lda,
-           const double *      B,
-           const unsigned int *ldb,
-           const double *      beta,
-           double *            C,
-           const unsigned int *ldc);
-  }
-
-
   template <typename NumberType, MemorySpace memorySpace>
   class Vector
   {
@@ -53,39 +21,6 @@ namespace dftefe
   private:
     NumberType *d_data;
   };
-
-  template <typename NumberType, MemorySpace memorySpace>
-  void
-  Vector<NumberType, memorySpace>::testDgemv()
-  {
-    char                transA = 'N';
-    char                transB = 'T';
-    const double        alpha = 1.0, beta = 0.0;
-    unsigned int        numberWaveFunctions = 10;
-    unsigned int        numberDofs          = 10;
-    std::vector<double> X, Y, output;
-    X.resize(100);
-    Y.resize(100);
-    output.resize(100);
-
-
-
-    dgemm_(&transA,
-           &transB,
-           &numberWaveFunctions,
-           &numberWaveFunctions,
-           &numberDofs,
-           &alpha,
-           &X[0],
-           &numberWaveFunctions,
-           &Y[0],
-           &numberWaveFunctions,
-           &beta,
-           &output[0],
-           &numberWaveFunctions);
-  }
-
-
 } // end of namespace dftefe
 
 
