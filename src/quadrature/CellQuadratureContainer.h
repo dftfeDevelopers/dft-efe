aaf29d86456bc12b1144398437f3aa2d3ce46a31
vishal-subbu <vishalsubbu97@gmail.com>
2021-11-22 15:27:28 -0500

    Added documentation

--- src/quadrature/CellQuadratureContainer.h
+++ src/quadrature/CellQuadratureContainer.h
@@ -12,43 +12,124 @@ namespace dftefe
 {
   namespace quadrature
   {
+
+    /**
+     * This class stores the quadrature points and corresponding JxW in each cell.
+     * This supports adaptive quadrature i.e each cell can have different quadrature rules.
+     * Further each cell can have arbitrary quadrature rule.
+     */
     class CellQuadratureContainer
     {
     public:
+
+      /**
+       * @brief Constructor for assigning each cell with a common quadrature rule.
+       * @param[in] quadratureRule The quadrature rule specifying the quad points in the parametric coordinates
+       * with its corresponding weights.
+       * @param[in] triangulation The triangulation that has information on the cell and its vertices
+       * @param[in] cellMapping cellMapping provides the the information on how the cell in real space is mapped to
+       * its parametric coordinates.  This is required to calculate the JxW values at each quad point
+       */
       CellQuadratureContainer(
         std::shared_ptr<const QuadratureRule>           quadratureRule,
         std::shared_ptr<const basis::TriangulationBase> triangulation,
         const basis::CellMappingBase &                  cellMapping);
 
+      /**
+       * @brief Constructor for assigning each cell with a common quadrature rule.
+       * @param[in] quadratureRule The quadrature rule specifying the quad points in the parametric coordinates
+       * with its corresponding weights.
+       * @param[in] triangulation The triangulation that has information on the cell and its vertices
+       * @param[in] cellMapping cellMapping provides the the information on how the cell in real space is mapped to
+       * its parametric coordinates.  This is required to calculate the JxW values at each quad point
+       */
       CellQuadratureContainer(
         std::vector<std::shared_ptr<const QuadratureRule>> quadratureRule,
         std::shared_ptr<const basis::TriangulationBase>    triangulation,
         const basis::CellMappingBase &                     cellMapping);
 
+      /**
+       * @brief Function that returns a vector containing the real coordinates of the quad points in all cells
+       *
+       * @returns  a vector of dftefe::utils::Point
+       */
       const std::vector<dftefe::utils::Point> &
       getRealPoints() const;
 
+      /**
+       * @brief Function that returns a vector containing the real coordinates of the
+       * quad points in cell corresponding to the cellId
+       *
+       * @param[n] cellId the id to the cell
+       * @returns  a vector of dftefe::utils::Point
+       */
       std::vector<dftefe::utils::Point>
       getCellRealPoints(const unsigned int cellId) const;
 
+      /**
+       * @brief Function that returns a vector containing the real coordinates of the
+       * quad points in cell corresponding to the cellId
+       *
+       * @param[n] cellId the id to the cell
+       * @returns  a vector of dftefe::utils::Point
+       */
       const std::vector<dftefe::utils::Point> &
       getCellParametricPoints(const unsigned int cellId) const;
 
+      /**
+       * @brief Function that returns a vector containing the weight of the
+       * quad points in cell corresponding to the cellId
+       *
+       * @param[n] cellId the id to the cell
+       * @returns  a vector of weights double
+       */
       const std::vector<double> &
       getCellQuadratureWeights(const unsigned int cellId) const;
 
+      /**
+       * @brief Function that returns a vector containing the real coordinates of the
+       * quad points in cell corresponding to the cellId
+       *
+       * @returns  a vector of dftefe::utils::Point
+       */
       const std::vector<double> &
       getJxW() const;
 
+      /**
+       * @brief Function that returns a vector containing the JxW of the
+       * quad points in cell corresponding to the cellId
+       *
+       * @param[n] cellId the id to the cell
+       * @returns  a vector of JxW (double)
+       */
       std::vector<double>
       getCellJxW(const unsigned int cellId) const;
 
+      /**
+       * @brief Function that returns the handle to quadrature rule corresponding to the
+       * the cell Id
+       *
+       * @param[n] cellId the id to the cell
+       * @returns  QuadratureRule
+       */
       const QuadratureRule &
       getQuadratureRule(const unsigned int cellId) const;
 
+      /**
+       * @brief  A function to return the total number of quadrature points in all the cells
+       *
+       * @returns  the number of quadrature points in all the cells
+       */
       size_type
       nQuadraturePoints() const;
 
+      /**
+       * @brief A function to returns the number of quadrature points in cell corresponding to the
+       * the cell Id
+       *
+       * @param[n] cellId the id to the cell
+       * @returns  number of quadrature points
+       */
       size_type
       nCellQuadraturePoints(const unsigned int cellId) const;
 
--- src/quadrature/QuadratureRule.h
+++ src/quadrature/QuadratureRule.h
@@ -9,22 +9,53 @@ namespace dftefe
 {
   namespace quadrature
   {
+    /*
+     *Class that stores the quadrature information. This class provides the location
+     * of the quadrature points in parametric coordinates and its corresponding weights
+     */
     class QuadratureRule
     {
     public:
+      /**
+       * @brief Constructor to create a tensor structured quadrature with arbitrary  1D points and weights.
+       * @param[in] dim the dimension of the cell.
+       * @param[in] points The parametric points on which the 1D quad points are located.
+       * @param[in] weights  the weight of the quad points
+       */
       QuadratureRule(const unsigned int               dim,
                      const std::vector<utils::Point> &points,
                      const std::vector<double> &      weights);
 
+      /**
+       * @brief A function to return the quad points in the parametric space.
+       *
+       * @returns  returns the vector of dftefe::utils::Point  containing the quad points
+       */
       virtual const std::vector<utils::Point> &
       getPoints() const;
 
+      /**
+       * @brief A function to return the 1D quad points in the parametric space.
+       * This function throws an error if this quadrature Rule is not tensor structured
+       *
+       * @returns  returns the vector of dftefe::utils::Point  containing the 1D quad points
+       */
       virtual const std::vector<utils::Point> &
       get1DPoints() const;
 
+      /**
+       * @brief A function to returns the weights of the quadrature points
+       *
+       * @returns  returns the vector doubles containing the weights
+       */
       virtual const std::vector<double> &
       getWeights() const;
-
+      /**
+       * @brief A function to return the 1D quad points in the parametric space.
+       * This function throws an error if this quadrature Rule is not tensor structured
+       *
+       * @returns  returns the vector of dftefe::utils::Point  containing the 1D quad points
+       */
       virtual const std::vector<double> &
       get1DWeights() const;
 
