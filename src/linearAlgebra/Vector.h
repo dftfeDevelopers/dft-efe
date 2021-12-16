6036795a1888c317e5618b03303ae91942e35197
Sambit Das <dsambit@umich.edu>
2021-12-16 14:36:08 -0500

    refactoring of Vector class into VectorStorage class in utils library with the original Vector class in linearAlgebra library now only handling the linear algebra aspects

--- src/linearAlgebra/Vector.h
+++ src/linearAlgebra/Vector.h
@@ -26,23 +26,15 @@
 #ifndef dftefeVector_h
 #define dftefeVector_h
 
-#include "utils/MemoryManager.h"
+#include <utils/VectorStorage.h>
 #include <utils/TypeConfig.h>
-#include <vector>
 namespace dftefe
 {
   namespace linearAlgebra
   {
     template <typename ValueType, dftefe::utils::MemorySpace memorySpace>
-    class Vector
+    class Vector : public dftefe::utils::VectorStorage<ValueType, memorySpace>
     {
-      typedef ValueType        value_type;
-      typedef ValueType *      pointer;
-      typedef ValueType &      reference;
-      typedef const ValueType &const_reference;
-      typedef ValueType *      iterator;
-      typedef const ValueType *const_iterator;
-
     public:
       Vector() = default;
 
@@ -65,95 +57,6 @@ namespace dftefe
        */
       explicit Vector(size_type size, ValueType initVal = 0);
 
-      /**
-       * @brief Destructor
-       */
-      ~Vector();
-
-
-      /**
-       * @brief Return iterator pointing to the beginning of point
-       * data.
-       *
-       * @returns Iterator pointing to the beginning of Vector.
-       */
-      iterator
-      begin();
-
-      /**
-       * @brief Return iterator pointing to the beginning of Vector
-       * data.
-       *
-       * @returns Constant iterator pointing to the beginning of
-       * Vector.
-       */
-      const_iterator
-      begin() const;
-
-      /**
-       * @brief Return iterator pointing to the end of Vector data.
-       *
-       * @returns Iterator pointing to the end of Vector.
-       */
-      iterator
-      end();
-
-      /**
-       * @brief Return iterator pointing to the end of Vector data.
-       *
-       * @returns Constant iterator pointing to the end of
-       * Vector.
-       */
-      const_iterator
-      end() const;
-
-
-      /**
-       * @brief Copy assignment operator
-       * @param[in] rhs the rhs Vector from which to copy
-       * @returns reference to the lhs Vector
-       */
-      Vector &
-      operator=(const Vector &rhs);
-
-
-      /**
-       * @brief Move assignment constructor
-       * @param[in] rhs the rhs Vector from which to move
-       * @returns reference to the lhs Vector
-       */
-      Vector &
-      operator=(Vector &&rhs) noexcept;
-
-      //      // This part does not work for GPU version, will work on this
-      //      until
-      //      // having cleaner solution.
-      //      /**
-      //       * @brief Operator to get a reference to a element of the Vector
-      //       * @param[in] i is the index to the element of the Vector
-      //       * @returns reference to the element of the Vector
-      //       * @throws exception if i >= size of the Vector
-      //       */
-      //      reference
-      //      operator[](size_type i);
-      //
-      //      /**
-      //       * @brief Operator to get a const reference to a element of the Vector
-      //       * @param[in] i is the index to the element of the Vector
-      //       * @returns const reference to the element of the Vector
-      //       * @throws exception if i >= size of the Vector
-      //       */
-      //      const_reference
-      //      operator[](size_type i) const;
-
-      /**
-       * @brief Deallocates and then resizes Vector with new size
-       * and initial value arguments
-       * @param[in] size size of the Vector
-       * @param[in] initVal initial value of elements of the Vector
-       */
-      void
-      resize(size_type size, ValueType initVal = 0);
 
       /**
        * @brief Compound addition for elementwise addition lhs += rhs
@@ -185,31 +88,6 @@ namespace dftefe
        */
       double
       lInfNorm() const;
-
-      /**
-       * @brief Returns the dimension of the Vector
-       * @returns size of the Vector
-       */
-      size_type
-      size() const;
-
-      /**
-       * @brief Return the raw pointer to the Vector
-       * @return pointer to data
-       */
-      ValueType *
-      data() noexcept;
-
-      /**
-       * @brief Return the raw pointer to the Vector without modifying the values
-       * @return pointer to const data
-       */
-      const ValueType *
-      data() const noexcept;
-
-    private:
-      ValueType *d_data = nullptr;
-      size_type  d_size = 0;
     };
 
     // helper functions
