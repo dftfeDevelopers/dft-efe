590b6e8a039f2e5c502e1298b0fe813f487ba3bb
Bikash Kanungo <bikash@umich.edu>
2022-01-06 11:02:25 -0500

    Add spline class and test

---
+++ src/utils/CubicSpline1D.h
@@ -0,0 +1,63 @@
+#ifndef dftefeCubicSpline1D_h
+#define dftefeCubicSpline1D_h
+
+#include "alglib/interpolation.h"
+#include <vector>
+#include <map>
+
+namespace dftefe {
+  namespace utils {
+
+    class CubicSpline1D {
+
+      public:
+
+	enum class BoundaryType {
+	  PERIODIC,
+	  PARABOLIC_TERMINATION,
+	  FIRST_DERIVATIVE,
+	  SECOND_DERIVATIVE
+	};
+
+	CubicSpline1D(const std::vector<double> & x,
+	    const std::vector<double> & y,
+	    const BoundaryType lBoundaryType = 
+	    BoundaryType::PARABOLIC_TERMINATION,
+	    const BoundaryType rBoundaryType = 
+	    BoundaryType::PARABOLIC_TERMINATION,
+	    const double lBoundaryValue = 0.0,
+	    const double rBoundaryValue = 0.0);
+
+	~CubicSpline1D() = default;
+
+	double getValue(const double & x) const;
+	std::vector<double>
+	  getValue(const std::vector<double> & x) const;
+	
+	double getFirstDerivativeValue(const double & x) const;
+	std::vector<double>
+	  getFirstDerivativeValue(const std::vector<double> & x) const ;
+
+	double getSecondDerivativeValue(const double & x) const;
+	std::vector<double>
+	  getSecondDerivativeValue(const std::vector<double> & x) const;
+
+      private:
+	static std::map<BoundaryType, alglib::ae_int_t> d_boundaryTypeToAlglibIntegerMap;
+	alglib::spline1dinterpolant d_spline;
+	std::vector<double> d_x;
+	std::vector<double> d_y;
+	unsigned int d_numPoints;
+	BoundaryType d_lBoundaryType;
+	BoundaryType d_rBoundaryType;
+	double d_lBoundaryValue;
+	double d_rBoundaryValue;
+
+    };
+
+  } // end of namespace utils
+
+
+} // end of namespace dftefe
+
+#endif // dftefeCubicSpline1D_h 
---
+++ test/utils/TestCubicSplineAlglib.py
@@ -0,0 +1,78 @@
+import reframe as rfm
+import reframe.utility.sanity as sn
+from reframe.utility.sanity import evaluate
+from reframe.core.backends import getlauncher
+import os
+DFTEFE_PATH=''
+if not 'DFTEFE_PATH' in os.environ:
+    raise Exception('''DFTEFE_PATH is not set. Please use export'''\
+                    '''DFTEFE_PATH=/path/to/dft-efe/parent/folder''')
+else:
+    DFTEFE_PATH = os.environ['DFTEFE_PATH']
+
+parser = rfm.utility.import_module_from_file(DFTEFE_PATH+"/test/Parser.py")
+cu = rfm.utility.import_module_from_file(DFTEFE_PATH+"/test/CompareUtil.py")
+ss = rfm.utility.import_module_from_file(DFTEFE_PATH+"/test/SetupSystems.py")
+cmflags = rfm.utility.import_module_from_file(DFTEFE_PATH+"/CMakeFlagsParser.py")
+"""
+Types of tags
+--------------
+compile: Make only tests
+run: Make and run tests
+unit: Tests that related to only a single function
+aggregate: Tests for a complex function
+slow: Tests requiring more than 30s of wall time
+fast: Tests requiring less than 30s of wall time
+cpu: Tests to be run on cpu only
+gpu: Tests to be run on gpu only
+both: Tests to be run on  both cpu and gpu
+serial: Serial tests (ideally requires no mpi or openmp)
+parallel: Parallel tests that requires mpi or openmp
+"""
+
+@rfm.simple_test
+class TestCubicSplineAlglib(rfm.CompileOnlyRegressionTest):
+    descr = '''Compile only test for cubic spline using alglib'''
+    build_system = 'CMake'
+    make_opts = ['TestCubicSplineAlglib']
+    sourcesdir = './src'
+    tagsDict = {'compileOrRun': 'compile', 'unitOrAggregate':
+                'aggregate', 'slowOrFast': 'fast', 'arch': 'cpu',
+                'serialOrParallel': 'serial'}
+    tags = {x.lower() for x in tagsDict.values()}
+    valid_systems = ss.getValidSystems(tagsDict['arch']) 
+    valid_prog_environs = ['*']
+    keep_files = []
+    config_opts = cmflags.getConfig(tagsDict['arch'])
+
+    @run_before('compile')
+    def set_compiler_flags(self):
+        self.build_system.make_opts = self.make_opts
+        self.build_system.config_opts = self.config_opts
+
+    @sanity_function
+    def validate_test(self):
+        hasWarning = True
+        hasError = True
+        msgWarning = "Found warning(s) while compiling."
+        msgError = "Found error(s) while compiling."
+        matches = evaluate(sn.findall(r'warning/i', evaluate(self.stdout)))
+        if len(matches) == 0:
+            hasWarning = False
+
+        matchesOut = evaluate(sn.findall(r'error/i', evaluate(self.stdout)))
+        matchesErr = evaluate(sn.findall(r'error/i', evaluate(self.stderr)))
+        if len(matchesOut) == 0 and len(matchesErr) == 0:
+            hasError = False
+        
+        hasTestPassed = not hasWarning and not hasError
+        msg = ""
+        if hasError:
+            msg = msgError
+
+        elif hasWarning:
+            msg = msgWarning
+
+        else:
+            msg = ""
+            return sn.assert_true(hasTestPassed, msg=msg)
