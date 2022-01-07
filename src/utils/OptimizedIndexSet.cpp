441dd93c2cf6f40054683171ca217c3b9befdf00
Bikash Kanungo <bikash@umich.edu>
2022-01-07 12:32:09 -0500

    Remove std::set default in OptimizedIndexSet

--- src/utils/OptimizedIndexSet.cpp
+++ src/utils/OptimizedIndexSet.cpp
@@ -25,103 +25,84 @@
 
 #include <utils/OptimizedIndexSet.h>
 #include <iterator>
-namespace dftefe {
-
-  namespace utils {
-
-    OptimizedIndexSet::OptimizedIndexSet(const std::set<global_size_type> & inputSet,
-	const double numRangesToSetSizeTol /*=0.1*/):
-      d_set(inputSet),
-      d_defaultToSTLSet(false),
-      d_contiguousRanges(0)
+#include <algorithm>
+namespace dftefe
+{
+  namespace utils
+  {
+    OptimizedIndexSet::OptimizedIndexSet(
+      const std::set<global_size_type> &inputSet)
+      : d_set(inputSet)
+      , d_contiguousRanges(0)
     {
-    
       std::set<global_size_type>::const_iterator itLastRange = d_set.begin();
-      std::set<global_size_type>::const_iterator itPrev = d_set.begin(); 
-      std::set<global_size_type>::const_iterator it = itPrev; 
-      it++; 
-      for(; it != d_set.end(); ++it)
-      {
-	bool isContiguous = ((*it - 1) == *(itPrev));
-	if(!isContiguous)
-	{
-	  d_contiguousRanges.push_back(*itLastRange);
-	  d_contiguousRanges.push_back(*(itPrev)+1);
-	  itLastRange = it;
-	}
-	itPrev= it;
-      }
+      std::set<global_size_type>::const_iterator itPrev      = d_set.begin();
+      std::set<global_size_type>::const_iterator it          = itPrev;
+      it++;
+      for (; it != d_set.end(); ++it)
+        {
+          bool isContiguous = ((*it - 1) == *(itPrev));
+          if (!isContiguous)
+            {
+              d_contiguousRanges.push_back(*itLastRange);
+              d_contiguousRanges.push_back(*(itPrev) + 1);
+              itLastRange = it;
+            }
+          itPrev = it;
+        }
       d_contiguousRanges.push_back(*itLastRange);
-      d_contiguousRanges.push_back(*(itPrev)+1);
+      d_contiguousRanges.push_back(*(itPrev) + 1);
 
-      d_numContiguousRanges = d_contiguousRanges.size()/2;
-      d_numEntriesBefore.resize(d_numContiguousRanges,0);
+      d_numContiguousRanges = d_contiguousRanges.size() / 2;
+      d_numEntriesBefore.resize(d_numContiguousRanges, 0);
       size_type cumulativeEntries = 0;
-      for(unsigned int i = 0; i < d_numContiguousRanges; ++i)
-      {
-	d_numEntriesBefore[i] = cumulativeEntries;
-	cumulativeEntries += d_contiguousRanges[2*i+1]-d_contiguousRanges[2*i];
-      }
-
-      if((d_numContiguousRanges+0.0)/d_set.size() > numRangesToSetSizeTol)
-	d_defaultToSTLSet = true;
-
+      for (unsigned int i = 0; i < d_numContiguousRanges; ++i)
+        {
+          d_numEntriesBefore[i] = cumulativeEntries;
+          cumulativeEntries +=
+            d_contiguousRanges[2 * i + 1] - d_contiguousRanges[2 * i];
+        }
     }
 
     void
     OptimizedIndexSet::getPosition(const global_size_type index,
-	size_type & pos,
-	bool & found)
+                                   size_type &            pos,
+                                   bool &                 found) const
     {
       found = false;
-      if(d_defaultToSTLSet)
-      { 
-	auto it = d_set.find(index);
-	if(it != d_set.end())
-	{
-	  found = true;
-	  pos = std::distance(d_set.begin(),it);
-	}
-      }
+      /*
+       * The logic used for finding an index is as follows:
+       * 1. Find the position of the element in d_contiguousRanges
+       *    which is greater than (strictly greater) the input index.
+       *    Let's call this position as upPos and the value at upPos as
+       *    upVal. The complexity of finding it is
+       *    O(log(size of d_contiguousRanges))
+       * 2. Since d_contiguousRanges stores pairs of startId and endId
+       *    (endId not inclusive) of contiguous ranges in d_set,
+       *    any index for which upPos is even (i.e., it corresponds to a
+       * startId) cannot belong to d_set. Why? Consider two consequtive ranges
+       * [k1,k2) and [k3,k4) where k1 < k2 < k3 < k4. If upVal for index
+       * corresponds to k3 (i.e., startId of a range), then (a) index does not
+       * lie in the [k3,k4) as index < upVal (=k3). (b) index cannot lie in
+       * [k1,k2), because if index lies in [k1,k2), then upVal should be k2 (not
+       * k3)
+       *  3. If upPos is odd (i.e, it corresponds to an endId), we find the
+       * relative position of index in that range. Subsequently, we determine
+       * the global position of index in d_set by adding the relative position
+       * to the number of entries in d_set prior to the range where index lies
+       */
 
-      else
-      { 
-	/*
-	 * The logic used for finding an index is as follows:
-	 * 1. Find the position of the element in d_contiguousRanges 
-	 *    which is greater than (strictly greater) the input index.
-	 *    Let's call this position as upPos and the value at upPos as
-	 *    upVal. The complexity of finding it is 
-	 *    O(log(size of d_contiguousRanges)) 
-	 * 2. Since d_contiguousRanges stores pairs of startId and endId
-	 *    (endId not inclusive) of contiguous ranges in d_set, 
-	 *    any index for which upPos is even (i.e., it corresponds to a startId) 
-	 *    cannot belong to d_set. Why?
-	 *    Consider two consequtive ranges [k1,k2) and [k3,k4) where
-	 *    k1 < k2 < k3 < k4. If upVal for index corresponds to k3 
-	 *    (i.e., startId of a range), then
-	 *    (a) index does not lie in the [k3,k4) as index < upVal (=k3). 
-	 *    (b) index cannot lie in [k1,k2), because if index lies in [k1,k2), then
-	 *    upVal should be k2 (not k3)
-	 *  3. If upPos is odd (i.e, it corresponds to an endId), we find the relative 
-	 *     position of index in that range. Subsequently, we determine the global 
-	 *     position of index in d_set by adding the relative position to
-	 *     the number of entries in d_set prior to the range where index lies
-	 */
-	   
-	auto up = std::upper_bound(d_contiguousRanges.begin(), 
-	    d_contiguousRanges.end(), index);
-	size_type upPos = std::distance(d_contiguousRanges.begin(), up);
-	if(upPos % 2 == 1)
-	{
-	  found = true;
-	  size_type rangeId = upPos/2;
-	  pos = d_numEntriesBefore[rangeId] + index - 
-	    d_contiguousRanges[upPos-1];
-	}
-      }
+      auto      up    = std::upper_bound(d_contiguousRanges.begin(),
+                                 d_contiguousRanges.end(),
+                                 index);
+      size_type upPos = std::distance(d_contiguousRanges.begin(), up);
+      if (upPos % 2 == 1)
+        {
+          found             = true;
+          size_type rangeId = upPos / 2;
+          pos =
+            d_numEntriesBefore[rangeId] + index - d_contiguousRanges[upPos - 1];
+        }
     }
-
   } // end of namespace utils
-
 } // end of namespace dftefe
--- src/utils/OptimizedIndexSet.h
+++ src/utils/OptimizedIndexSet.h
@@ -28,53 +28,53 @@
 #include <utils/TypeConfig.h>
 #include <set>
 #include <vector>
-namespace dftefe {
-
-  namespace utils {
-
+namespace dftefe
+{
+  namespace utils
+  {
     /*
      * @brief Class to create an optimized index set which
      * creates contiguous sub-ranges within an index set for faster
      * search operation. This is useful when the number of contiguous sub-ranges
-     * are fewer compared to the size of the index set. If the number of contiguous 
-     * sub-ranges competes with the size of the index set (i.e., the index set 
-     * is very random) then it default to the behavior of an std::set. The default to STL
-     * set is governed by the numRangesToSetSizeTol value. That is, if the ratio of 
-     * number of contiguous ranges to the size of index set exceeds numRangesToSetSizeTol,
-     * it defaults to the behavior of STL set. The default value for numRangesToSetSizeTol is 0.1 
+     * are fewer compared to the size of the index set. If the number of
+     * contiguous sub-ranges competes with the size of the index set (i.e., the
+     * index set is very random) then it default to the behavior of an std::set.
+     * The default to STL set is governed by the numRangesToSetSizeTol value.
+     * That is, if the ratio of number of contiguous ranges to the size of index
+     * set exceeds numRangesToSetSizeTol, it defaults to the behavior of STL
+     * set. The default value for numRangesToSetSizeTol is 0.1
      */
-    class OptimizedIndexSet 
+    class OptimizedIndexSet
     {
-      public:
-      OptimizedIndexSet(const std::set<global_size_type> & inputSet,
-	  const double numRangesToSetSizeTol = 0.1);
+    public:
+      OptimizedIndexSet(const std::set<global_size_type> &inputSet);
       ~OptimizedIndexSet() = default;
-     
+
       void
       getPosition(const global_size_type index,
-	 size_type & pos,
-	 bool & found);
+                  size_type &            pos,
+                  bool &                 found) const;
 
-      private:
-       bool d_defaultToSTLSet;
-       std::set<global_size_type> d_set;
-       size_type d_numContiguousRanges;
-       
-       /*
-        * Vector of size 2*(d_numContiguousRanges in d_set). 
-        * The entries are arranged as:
-        * <contiguous range1 startId> <continguous range1 endId> <contiguous range2 startId> <continguous range2 endId> ...
-        * NOTE: The endId is one past the lastId in the continguous range
-	*/
-       std::vector<global_size_type> d_contiguousRanges;
-       
-       /// Vector of size d_numContiguousRanges which stores the accumulated 
-       /// number of elements in d_set prior to the i-th contiguous range
-       std::vector<size_type> d_numEntriesBefore;
+    private:
+      std::set<global_size_type> d_set;
+      size_type                  d_numContiguousRanges;
+
+      /*
+       * Vector of size 2*(d_numContiguousRanges in d_set).
+       * The entries are arranged as:
+       * <contiguous range1 startId> <continguous range1 endId> <contiguous
+       * range2 startId> <continguous range2 endId> ... NOTE: The endId is one
+       * past the lastId in the continguous range
+       */
+      std::vector<global_size_type> d_contiguousRanges;
+
+      /// Vector of size d_numContiguousRanges which stores the accumulated
+      /// number of elements in d_set prior to the i-th contiguous range
+      std::vector<size_type> d_numEntriesBefore;
     };
 
   } // end of namespace utils
 
 } // end of namespace dftefe
 
-#endif //dftefeOptimizedSet_h
+#endif // dftefeOptimizedSet_h
