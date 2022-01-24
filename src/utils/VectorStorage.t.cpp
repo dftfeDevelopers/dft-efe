d0158d222c26abd67c5fe4e05bc333cc3bd5e54a
Bikash Kanungo <bikash@umich.edu>
2022-01-24 14:41:11 -0500

    Rename transfer to transferTo in VectorStorage

--- src/utils/VectorStorage.t.cpp
+++ src/utils/VectorStorage.t.cpp
@@ -199,7 +199,7 @@ namespace dftefe
     template <typename ValueType, dftefe::utils::MemorySpace memorySpace>
     template <dftefe::utils::MemorySpace memorySpaceDst>
     void
-    VectorStorage<ValueType, memorySpace>::transfer(
+    VectorStorage<ValueType, memorySpace>::transferTo(
       VectorStorage<ValueType, memorySpaceDst> &dstVectorStorage) const
     {
       throwException<DomainError>(
@@ -208,31 +208,31 @@ namespace dftefe
       MemoryTransfer<memorySpaceDst, memorySpace> memoryTransfer;
       memoryTransfer.copy(d_size, dstVectorStorage.begin(), d_data);
     }
-      
+
     template <typename ValueType, dftefe::utils::MemorySpace memorySpace>
     template <dftefe::utils::MemorySpace memorySpaceDst>
-      void
-      VectorStorage<ValueType, memorySpace>::transfer(
-        VectorStorage<ValueType, memorySpaceDst> &dstVectorStorage,
-	const size_type N,
-	const size_type srcOffset,
-	const size_type dstOffset) const
-      {
+    void
+    VectorStorage<ValueType, memorySpace>::transferTo(
+      VectorStorage<ValueType, memorySpaceDst> &dstVectorStorage,
+      const size_type                           N,
+      const size_type                           srcOffset,
+      const size_type                           dstOffset) const
+    {
+      throwException<DomainError>(
+        srcOffset + N <= d_size,
+        "The offset and copy size specified for the source VectorStorage"
+        " is out of range for it.");
 
-	throwException<DomainError>(srcOffset + N <= d_size, 
-	    "The offset and copy size specified for the source VectorStorage"
-	    " is out of range for it.");
+      throwException<DomainError>(
+        dstOffset + N <= dstVectorStorage.size(),
+        "The offset and size specified for the destination VectorStorage"
+        " is out of range for it.");
 
-	throwException<DomainError>(
-	    dstOffset + N <= dstVectorStorage.size(), 
-	    "The offset and size specified for the destination VectorStorage"
-	    " is out of range for it.");
-      
-	MemoryTransfer<memorySpaceDst, memorySpace> memoryTransfer;
-        memoryTransfer.copy(N, dstVectorStorage.begin() + dstOffset, 
-	  this->begin()+srcOffset);
-
-      }
+      MemoryTransfer<memorySpaceDst, memorySpace> memoryTransfer;
+      memoryTransfer.copy(N,
+                          dstVectorStorage.begin() + dstOffset,
+                          this->begin() + srcOffset);
+    }
 
 
 
