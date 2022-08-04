6e00c283f1e2fd3f8ec5c061a720451cbf191936
Bikash Kanungo <bikash@umich.edu>
2022-08-05 01:24:59 +0530

    Add copy to and from STL in MemoryStorage

--- src/utils/MemoryStorage.h
+++ src/utils/MemoryStorage.h
@@ -210,6 +210,8 @@ namespace dftefe
        *  MemoryStorage. It must be pre-allocated appropriately
        * @param[out] dstMemoryStorage reference to the destination
        *  MemoryStorage with the data copied into it
+       * @throw utils::LengthError exception if the size of dstMemoryStorage is
+       * less than underlying MemoryStorage
        */
       template <dftefe::utils::MemorySpace memorySpaceDst>
       void
@@ -236,6 +238,10 @@ namespace dftefe
        *  MemoryStorage to which we need to copy data
        * @param[out] dstMemoryStorage reference to the destination
        *  MemoryStorage with the data copied into it
+       * @throw utils::LengthError exception if the size of dstMemoryStorage is
+       * less than N + dstOffset
+       * @throw utils::LengthError exception if the size of underlying
+       * MemoryStorage is less than N + srcOffset
        */
       template <dftefe::utils::MemorySpace memorySpaceDst>
       void
@@ -255,6 +261,8 @@ namespace dftefe
        *  from which to copy
        * @param[in] srcMemoryStorage reference to the source
        *  MemoryStorage
+       * @throw utils::LengthError exception if the size of underlying
+       * MemoryStorage is less than size of srcMemoryStorage
        */
       template <dftefe::utils::MemorySpace memorySpaceSrc>
       void
@@ -281,6 +289,10 @@ namespace dftefe
        *  MemoryStorage from which we need to copy data
        * @param[in] dstOffset offset relative to the start of the destination
        *  MemoryStorage to which we need to copy data
+       * @throw utils::LengthError exception if the size of srcMemoryStorage is
+       * less than N + srcOffset
+       * @throw utils::LengthError exception if the size of underlying
+       * MemoryStorage is less than N + dstOffset
        */
       template <dftefe::utils::MemorySpace memorySpaceSrc>
       void
@@ -294,7 +306,8 @@ namespace dftefe
        * This provides a seamless interface to copy back and forth between
        * memory spaces, including between the same memory spaces.
        *
-       * @note The destination pointer must be pre-allocated appropriately
+       * @note The destination pointer must be pre-allocated to be
+       * at least of the size of the MemoryStorage
        *
        * @tparam memorySpaceDst memory space of the destination pointer
        * @param[in] dst pointer to the destination. It must be pre-allocated
@@ -313,7 +326,8 @@ namespace dftefe
        * transfer from a specific portion of the source MemoryStorage to a
        * specific portion of the destination pointer.
        *
-       * @note The destination pointer must be pre-allocated appropriately
+       * @note The destination pointer must be pre-allocated to be at least
+       * of the size N + dstOffset
        *
        * @tparam memorySpaceDst memory space of the destination pointer
        * @param[in] dst pointer to the destination. It must be pre-allocated
@@ -325,6 +339,8 @@ namespace dftefe
        * @param[in] dstOffset offset relative to the start of the destination
        *  pointer to which we need to copy data
        * @param[out] dst pointer to the destination with the data copied into it
+       * @throw utils::LengthError exception if the size of the MemoryStorage is
+       * less than N + srcOffset
        */
       template <dftefe::utils::MemorySpace memorySpaceDst>
       void
@@ -339,7 +355,8 @@ namespace dftefe
        * This provides a seamless interface to copy back and forth between
        * memory spaces, including between the same memory spaces.
        *
-       * @note The MemoryStorage must be pre-allocated appropriately
+       * @note The src pointer must point to a memory chunk that is at least the
+       * size of the MemoryStorage
        *
        * @tparam memorySpaceSrc memory space of the source pointer
        *  from which to copy
@@ -357,7 +374,8 @@ namespace dftefe
        * provides transfer from a specific portion of the source memory
        * to a specific portion of the destination MemoryStorage.
        *
-       * @note The MemoryStorage must be pre-allocated appropriately
+       * @note The src pointer must point to a memory chunk that is at least
+       * the size of N + srcOffset
        *
        * @tparam memorySpaceSrc memory space of the source pointer
        *  from which to copy
@@ -368,6 +386,8 @@ namespace dftefe
        *  pointer from which we need to copy data
        * @param[in] dstOffset offset relative to the start of the destination
        *  MemoryStorage to which we need to copy data
+       * @throw utils::LengthError exception if the size of the MemoryStorage is
+       * less than N + dstOffset
        */
       template <dftefe::utils::MemorySpace memorySpaceSrc>
       void
@@ -376,10 +396,128 @@ namespace dftefe
                const size_type  srcOffset,
                const size_type  dstOffset);
 
+      /**
+       * @brief Copies the data to a C++ STL vector, which always resides in
+       * the CPU. This provides a seamless interface to copy from any memory
+       * space to a C++ STL vector, including the case where source memory space
+       * is HOST (i.e., it resides on the CPU)
+       *
+       * @param[in] dst reference to the destination C++ STL vector to which
+       *  the data needs to be copied.
+       * @param[out] dst reference to the destination C++ STL vector with
+       * the data copied into it
+       * @note If the size of the dst vector is less than the the size of
+       * the underlying MemoryStorage, it will be resized. Thus, for performance
+       * reasons, it is recommened that the dst STL vector be pre-allocated
+       * appropriately.
+       */
+      void
+      copyTo(std::vector<ValueType> &dst) const;
+
+      /**
+       * @brief Copies the data to a C++ STL vector, which always resides in
+       * the CPU. This provides a seamless interface to copy from any memory
+       * space to a C++ STL vector, including the case where source memory space
+       * is HOST (i.e., it resides on the CPU).
+       * This is a more granular version of the above copyToSTL function as it
+       * provides transfer from a specific portion of the MemoryStorage
+       * to a specific portion of the destination STL vector.
+       *
+       * @param[in] dst reference to the destination C++ STL vector to which
+       *  the data needs to be copied.
+       * @note If the size of the dst vector is less than the the size of
+       * the underlying memory storage, it will be resized. Thus, for
+       * performance reasons it is recommened to should be allocated
+       * appropriately.
+       * @param[in] N number of entries of the source MemoryStorage
+       *  that needs to be copied to the destination pointer
+       * @param[in] srcOffset offset relative to the start of the source
+       *  MemoryStorage from which we need to copy data
+       * @param[in] dstOffset offset relative to the start of the STL vector
+       *  to which we need to copy data
+       * @param[out] dst reference to the destination C++ STL vector with
+       * the data copied into it
+       * @throw utils::LengthError exception if the size of the MemoryStorage is
+       * less than N + srcOffset
+       * @note If the size of the dst vector is less N + srcOffset, it will be resized.
+       *  Thus, for performance reasons, it is recommened that the dst STL
+       * vector be allocated appropriately.
+       */
+      void
+      copyTo(std::vector<ValueType> &dst,
+             const size_type         N,
+             const size_type         srcOffset,
+             const size_type         dstOffset) const;
+
+      /**
+       * @brief Copies data from a C++ STL vector to the MemoryStorage object,
+       * which always resides on a CPU. This provides a seamless interface to
+       * copy from any memory space, including the case where the same memory
+       * spaces is HOST(i.e., the MemoryStorage is on CPU).
+       *
+       * @param[in] src const reference to the source C++ STL vector from which
+       *  the data needs to be copied.
+       *
+       * @throw utils::LengthError exception if the size of the MemoryStorage is
+       * less than the size of the src
+       */
+      void
+      copyFrom(const std::vector<ValueType> &src);
+
+      /**
+       * @brief Copies data from a C++ STL vector to the MemoryStorage object,
+       * which always resides on a CPU. This provides a seamless interface to
+       * copy from any memory space, including the case where the same memory
+       * spaces is HOST(i.e., the MemoryStorage is on CPU). This is a more
+       * granular version of the above copyFromSTL function as it provides
+       * transfer from a specific portion of the source STL vector to to a
+       * specific portion of the destination MemoryStorage.
+       *
+       * @param[in] src const reference to the source C++ STL vector from which
+       *  the data needs to be copied.
+       * @param[in] N number of entries of the source pointer
+       *  that needs to be copied to the destination MemoryStorage
+       * @param[in] srcOffset offset relative to the start of the source STL
+       *  vector from which we need to copy data
+       * @param[in] dstOffset offset relative to the start of the destination
+       *  MemoryStorage to which we need to copy data
+       * @throw utils::LengthError exception if the size of src is less than
+       * N + srcOffset
+       * @throw utils::LengthError exception if the size of the MemoryStorage
+       * is less thant N + dstOffset
+       *
+       */
+      void
+      copyFrom(const std::vector<ValueType> &src,
+               const size_type               N,
+               const size_type               srcOffset,
+               const size_type               dstOffset);
+
+
     private:
       ValueType *d_data = nullptr;
       size_type  d_size = 0;
     };
+
+    //
+    // helper functions
+    //
+
+    /**
+     * @brief Create a MemoryStorage object from an input C++ STL vector
+     * @param[in] src Input C++ STL vector from which the MemoryStorage
+     *  object is to be created
+     * @return A MemoryStorage object containing the data in the input C++
+     *  STL vector
+     * @tparam ValueType Datatype of the underlying data in MemoryStorage
+     *  as well as C++ STL vector (e.g., int, double, float, complex<double>,
+     *  etc)
+     * @tparam memorySpaceDst MemorySpace (e.g. HOST, DEVICE, HOST_PINNED, etc)
+     * where the output MemoryStorage object should reside
+     */
+    template <ValueType, utils::MemorySpace memorySpaceDst>
+    MemoryStorage<ValueType, memorySpaceDst>
+    memoryStorageFromSTL(const std::vector<ValueType> &src);
 
   } // namespace utils
 } // end of namespace dftefe
