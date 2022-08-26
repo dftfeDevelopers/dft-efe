7501922b8a68a2421dadfc0cd7a5d1a164216886
Vishal Subramanian <vishalsubbu97@gmail.com>
2022-08-26 09:29:07 -0600

    minor fixes

--- src/linearAlgebra/BlasLapack.h
+++ src/linearAlgebra/BlasLapack.h
@@ -84,12 +84,14 @@ namespace dftefe
       // i have neglected incx & incy parameters
       /**
        * @brief Template for computing the multiplicative inverse of all the elements of x, does not check if any element is zero
+       * computes \f $ y[i] = \frac{alpha}{x[i]} $ \f 
        * @param[in] n size of each vector
+       * @param[in] alpha scalr input for the numerator 
        * @param[in] x input vector
        * @param[out] y output vector
        * @param[in] context Blas context for GPU operations
        *
-       * @return \f$ l_{\inf} \f$  norms of all the vectors
+       * @return orms of all the vectors
        */
       template <typename ValueType1,
                 typename ValueType2,
