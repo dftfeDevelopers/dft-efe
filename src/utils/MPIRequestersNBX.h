77568f1c17a31293d894bee11cfd27298851ebef
Bikash Kanungo <bikash@umich.edu>
2023-06-01 16:40:05 -0400

    Fix compilation issues in MPIRequestersNBX

--- src/utils/MPIRequestersNBX.h
+++ src/utils/MPIRequestersNBX.h
@@ -192,14 +192,18 @@ namespace dftefe
 
         /**
          * Buffers for receiving requests.
-         *
+         * We use a vector of unique pointers because that
+         * guarantees that the buffers themselves
+         * are never moved around in memory, even if the vector is
+         * resized and consequently its elements (the pointers) are moved
+         * around.
          */
-        std::vector<int> d_recvBuffers;
+        std::vector<std::unique_ptr<int>> d_recvBuffers;
 
         /**
          * Requests for receiving requests.
          */
-        std::vector<std::unique_ptr<MPI_Request>> d_recvRequests;
+        std::vector<std::unique_ptr<MPIRequest>> d_recvRequests;
 
         //
         // request for barrier
